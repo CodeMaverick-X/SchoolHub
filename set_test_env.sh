6656f09ab553b5f0c052f32b9f6e17da327e07af
reinhard <reinhard2057@gmail.com>
2023-04-04 11:37:33 +0000

    added caution comment for set enviroment scripts

--- set_test_env.sh
+++ set_test_env.sh
@@ -2,4 +2,5 @@
 # set env variable for production
 
 bash -c 'export SH_USER=sh_test_user; export SH_PWD=Sch_t3st_#3#_pwd; export SH_HOST=localhost; export SH_DATABASE=sh_test_db; export SH_ENV=test; exec bash'
-
+# Note: its adviced to manualy copy the env variables and set them, because executing this script creates multiple 
+# sub shells.
